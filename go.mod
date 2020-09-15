eef8bd2c25d8a31e9d4c7c7ae8f2546e97ad871a
neglect-yp <grandis.hebitombo@gmail.com>
2020-09-15 15:52:23 +0900

    list imported proto files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/neglect-yp/ls-imported-proto
 
 go 1.15
+
+require github.com/emicklei/proto v1.9.0
--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/emicklei/proto"
+)
+
+var (
+	memo = map[string]struct{}{}
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	flag.Parse()
+	filename := flag.Arg(0)
+
+	if err := walk(filename); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func walk(filename string) (err error) {
+	if _, ok := memo[filename]; ok {
+		return nil
+	}
+	memo[filename] = struct{}{}
+
+	fmt.Println(filename)
+
+	reader, err := os.Open(filename)
+	if err != nil {
+		if _, ok := err.(*os.PathError); ok {
+			return nil
+		}
+		return err
+	}
+
+	defer func() {
+		err = reader.Close()
+	}()
+
+	parser := proto.NewParser(reader)
+	definition, err := parser.Parse()
+	if err != nil {
+		return err
+	}
+
+	proto.Walk(definition, proto.WithImport(handleImport))
+
+	return nil
+}
+
+func handleImport(s *proto.Import) {
+	if err := walk(s.Filename); err != nil {
+		log.Fatal(err)
+	}
 }
